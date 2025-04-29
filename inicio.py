491c35df065972ce7cb050dd3890aa9a6fe3589d
nicoraww <94559409+nicoraww@users.noreply.github.com>
2025-04-29 16:17:09 -0500

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -2,19 +2,4 @@
 import streamlit as st
 import random
 
-st.title("📄 Página O")
-st.markdown("**Palabras que empiezan con O**")
-
-# Lista de ejemplos
-words_o = [
-    "Oso", "Olla", "Oro", "Oveja",
-    "Olivo", "Oído", "Otoño", "Ópera",
-    "Orgullo", "Orquesta"
-]
-
-if st.button("🎲 Mostrar palabra aleatoria con O"):
-    choice = random.choice(words_o)
-    st.success(f"**{choice}**")
-
-st.write("Lista completa:")
-st.write(words_o)
+st.title("Vocales")
